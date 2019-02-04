4b1ee2ff1014167328c5eaa0d1b34c08a593ac40
danielsalgadop <daniel.salgado.p@gmail.com>
2019-02-04 09:38:29 +0100

    login route

---
+++ src/Infrastructure/SacController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Infrastructure;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SacController extends AbstractController
+{
+    /**
+     * @Route("/login/", name="login", methods={"GET"})
+     */
+    public function index()
+    {
+//        return new Response("login with facebook");
+        return $this->json([
+            'message' => 'Welcome to your login page!',
+            'path' => 'src/Controller/SacController.php',
+        ]);
+    }
+}
