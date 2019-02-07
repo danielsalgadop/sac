caa596ee7027852d8e69831ebab871c71f22ccd7
danielsalgadop <daniel.salgado.p@gmail.com>
2019-02-07 23:13:02 +0100

    not working

--- src/Infrastructure/SacController.php
+++ src/Infrastructure/SacController.php
@@ -8,51 +8,92 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SacController extends AbstractController
 {
+//appId      : '361174884467644',
+
+//version    : 'v3.2'
+
     /**
      * @Route("/login", name="login", methods={"GET"})
      */
     public function login()
     {
         return new Response("<html>
-<head>    <script>
+        <head></head><body>
+
+
+<script>
   window.fbAsyncInit = function() {
-      FB.init({
-appId      : '361174884467644',
-cookie     : true,
-xfbml      : true,
-version    : 'v3.2'
-});
+    FB.init({
+      appId      : '361174884467644',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.2'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
 
-FB.AppEvents.logPageView();
-
-};
-
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
      js = d.createElement(s); js.id = id;
      js.src = \"https://connect.facebook.net/en_US/sdk.js\";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
-    
-    
-
 </script>
-</head>
-<body>
 
-login with facebook         
-<script>
+
+        </body>
+</html>
+    ");
+    }
+    /**
+     * @Route("/loginStatus", name="login", methods={"GET"})
+     */
+    public function loginStatus()
+    {
+        return new Response("<html>
+        <head></head><body>
+        <script>
+        
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-    console.log(response);
+    console.log(\"dasf\");
 });
 
 </script>
-</body>
-   </html>
+
+        </body>
+</html>
     ");
     }
+
+
+
+
+    /**
+     * @Route("/loginOk", name="loginOk", methods={"GET"})
+     */
+    public function loginOk(){
+        return new Response("FB loginOk");
+    }
+
+    /**
+     * @Route("/privacy", name="privacy", methods={"GET"})
+     */
+    public function privacy(){
+        return new Response("FB privacy");
+    }
+
+    /**
+     * @Route("/conditions", name="conditions", methods={"GET"})
+     */
+    public function conditions(){
+        return new Response("FB conditions");
+    }
+
+
 }
 
 
