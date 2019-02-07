4df135313cb89da0fff31c4fd47094193424b321
danielsalgadop <daniel.salgado.p@gmail.com>
2019-02-07 10:10:41 +0100

    not working

--- src/Infrastructure/SacController.php
+++ src/Infrastructure/SacController.php
@@ -13,6 +13,46 @@ class SacController extends AbstractController
      */
     public function login()
     {
-        return new Response("login with facebook");
+        return new Response("<html>
+<head>    <script>
+  window.fbAsyncInit = function() {
+      FB.init({
+appId      : '361174884467644',
+cookie     : true,
+xfbml      : true,
+version    : 'v3.2'
+});
+
+FB.AppEvents.logPageView();
+
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = \"https://connect.facebook.net/en_US/sdk.js\";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+    
+    
+
+</script>
+</head>
+<body>
+
+login with facebook         
+<script>
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+    console.log(response);
+});
+
+</script>
+</body>
+   </html>
+    ");
     }
 }
+
+
