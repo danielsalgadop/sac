044b83446b4ea21019358c98b33768cd8ba48aeb
danielsalgadop <daniel.salgado.p@gmail.com>
2019-03-01 11:12:17 +0100

    added Symfonys command SearchOwnerByfbDelegatedOrException.php

---
+++ src/Command/SearchOwnerByfbDelegatedOrException.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Domain\Repository\OwnerRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SearchOwnerByfbDelegatedOrException extends Command
+{
+    protected static $defaultName = "app:searchOwnerByfbDelegatedOrException ";
+    private $ownerRepo;
+
+    public function __construct(OwnerRepositoryInterface $ownerRepo)
+    {
+        parent::__construct();
+        $this->ownerRepo = $ownerRepo;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription("Test");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        dump($this->ownerRepo->searchOwnerByfbDelegatedOrException('fb_delegated_1'));
+    }
+}
