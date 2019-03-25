9c32cd28910fdf15152602684116f8e6adc66048
danielsalgadop <daniel.salgado.p@gmail.com>
2019-03-25 11:25:09 +0100

    Owner\Create

---
+++ src/Infrastructure/Action/MySQLActionRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+
+namespace App\Infrastructure\Action;
+
+
+
+    use App\Domain\Entity\Friend;
+    use App\Domain\Repository\Action\ActionRepository;
+    use Doctrine\ORM\EntityManagerInterface;
+
+class MysqlActionRepository implements ActionRepository
+{
+    private $em;
+//    private $friendRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+//        $this->friendRepository = $this->em->getRepository(Friend::class);   // Esto es lo que me ha traido quebradero de cabeza
+    }
+
+//    public function findAll()
+//    {
+//        return $this->friendRepository->findAll();
+//    }
+
+//    public function searchByfbDelegated(string $fbDelegated)
+//    {
+//        return $this->friendRepository->findOneBy(['fbDelegated' =>$fbDelegated]);
+//    }
+
+//    public function searchById(int $id)
+//    {
+//        return $this->friendRepository->findOneBy(['id' =>$id]);
+//    }
+
+    public function save(Friend $owner)
+    {
+        try {
+            $this->em->persist($owner);
+            $this->em->flush();   // TODO: mover el flush al Controller
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function getIdFromfbDelegated(string $fbDelegated)
+    {
+//        return 1;
+        // usando CreateQuery
+        $query = $this->em->createQuery('SELECT id FROM App\Domain\Entity\Friend o where o.fb_delegated = ":fbDelegated"')->setParameter('fbDelegated', $fbDelegated);
+        // usando QueryBuilder
+//        $friendRepository = $this->em->getRepository(Friend::class);
+//        $query = $friendRepository->createQueryBuilder('o')->select('id')->from(Friend::class, 'o')->where('o.fb_delegated = '.$fbDelegated);
+        // comun a CreateQuery (DDL) o QueryBuilder
+        $id = $query->getResult();
+        return $id;
+    }
+
+    public function searchFriendByfbDelegatedOrException(string $fbDelegated)
+    {
+
+        $owner = $this->friendRepository->findOneBy(['fbDelegated' => $fbDelegated]);
+        if (!$owner) {
+            throw new \Exception("Friend not found by firendFbDelegated [".$fbDelegated."]");
+        }
+
+
+
+        if ($owner === null) {
+            throw new \Exception("Non-existing fbDelegated");
+        }
+        return $owner;
+    }
+}
+
+}
---
+++ src/Infrastructure/Owner/Command/Create.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Infrastructure\Owner\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Application\Command\Owner\CreateOwnerCommand;
+use App\Application\CommandHandler\Owner\CreateOwnerHandler;
+
+
+class Create extends ContainerAwareCommand
+{
+    protected static $defaultName = 'app:Owner:Create';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('name', InputArgument::REQUIRED, '(string) Owners Name ')
+            ->addArgument('fbDelegated', InputArgument::REQUIRED, '(string) Owners fbDelegated ')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $fbDelegated = $input->getArgument('fbDelegated');
+        $name = $input->getArgument('name');
+
+        $ownerRepository = $this->getContainer()->get('app.repository.owner');
+        $createOwnerCommand = new CreateOwnerCommand($name, $fbDelegated);
+        $createOwnerHandler = new CreateOwnerHandler($ownerRepository);
+        $createOwnerHandler->handle($createOwnerCommand);
+
+
+        $io->success('Owner Created with name ['.$name.'] identified by fbDelegated ['.$fbDelegated.']');
+    }
+}
