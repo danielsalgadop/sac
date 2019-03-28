ef408d60cb184bc6a0dab91a67b3191d342bbfa8
danielsalgadop <daniel.salgado.p@gmail.com>
2019-03-28 14:57:20 +0100

    GetListThingsByOwner

--- src/Infrastructure/Owner/Command/GetListThingsByOwner.php
+++ src/Infrastructure/Owner/Command/GetListThingsByOwner.php
@@ -4,12 +4,10 @@ namespace App\Infrastructure\Owner\Command;
 
 use App\Application\Command\Owner\GetListThingsByOwnerCommand;
 use App\Application\CommandHandler\Owner\GetListThingsByOwnerHandler;
-use App\Domain\Repository\OwnerRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
 use App\Application\Command\Owner\SearchOwnerByFbDelegatedCommand;
 use App\Application\CommandHandler\Owner\SearchOwnerByFbDelegatedHandler;
@@ -17,12 +15,14 @@ use App\Application\CommandHandler\Owner\SearchOwnerByFbDelegatedHandler;
 class GetListThingsByOwner extends Command
 {
     protected static $defaultName = 'app:Owner:GetListThingsByOwner';
-    private $ownerRepository;
+    private $searchOwnerByFbDelegatedHandler;
+    private $getListThingsByOwnerHandler;
 
-    public function __construct(OwnerRepository $ownerRepository)
+    public function __construct(SearchOwnerByFbDelegatedHandler $searchOwnerByFbDelegatedHandler, GetListThingsByOwnerHandler $getListThingsByOwnerHandler)
     {
         parent::__construct();
-        $this->ownerRepository = $ownerRepository;
+        $this->searchOwnerByFbDelegatedHandler = $searchOwnerByFbDelegatedHandler;
+        $this->getListThingsByOwnerHandler = $getListThingsByOwnerHandler;
     }
 
 
@@ -30,24 +30,17 @@ class GetListThingsByOwner extends Command
     {
         $this
             ->setDescription('Given an fb_delegated returns list of things')
-            ->addArgument('fbDelegated', InputArgument::REQUIRED, '(string) fb_delegated')
+            ->addArgument('ownerFbDelegated', InputArgument::REQUIRED, '(string) fb_delegated')
             ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $fbDelegated = $input->getArgument('fbDelegated');
-
-        $searchOwnerByFbDelegatedCommand = new SearchOwnerByFbDelegatedCommand($fbDelegated);
-        $searchOwnerByFbDelegatedHandler = new SearchOwnerByFbDelegatedHandler($this->ownerRepository);
-
-        $owner = $searchOwnerByFbDelegatedHandler->handle($searchOwnerByFbDelegatedCommand);
-
-        $getListThingsByOwnerCommand = new GetListThingsByOwnerCommand($owner);
-        $getListThingsByOwnerHandler = new GetListThingsByOwnerHandler();
 
 
-        $things = $getListThingsByOwnerHandler->handle($getListThingsByOwnerCommand);
+        $owner = $this->searchOwnerByFbDelegatedHandler->handle(new SearchOwnerByFbDelegatedCommand($input->getArgument('ownerFbDelegated')));
+
+        $things = $this->getListThingsByOwnerHandler->handle(new GetListThingsByOwnerCommand($owner));
         dd($things);
     }
 }
