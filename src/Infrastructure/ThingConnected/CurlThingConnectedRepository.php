6bbe48e79ff43cd03ae25ae10648eaa2fcb5b1ca
danielsalgadop <daniel.salgado.p@gmail.com>
2019-04-02 14:58:02 +0200

    trying to use parameters in CurlThingConnectedRepository, but couldnt

--- src/Infrastructure/ThingConnected/CurlThingConnectedRepository.php
+++ src/Infrastructure/ThingConnected/CurlThingConnectedRepository.php
@@ -7,19 +7,35 @@ use App\Domain\Repository\ThingConnectedRepository;
 
 class CurlThingConnectedRepository implements ThingConnectedRepository
 {
-    const ENDPOINT = 'localhost';
-    const PORT = '8001';
+//    private $endpoint = "localhost";
+//    private $port = "8001";
 
-    function sendCurl($id, $thing_user_name, $thing_password)
+    private $endpoint;
+    private $port;
+
+
+//    public function __construct(string $endpoint, int $port)
+    public function __construct()
+//    public function __construct(string $endpoint)
     {
+
+//        $this->endpoint = $endpoint;
+        $this->endpoint = "localhost";
+        $this->port = 8001;
+//        $this->port = $port;
+    }
+
+    private function sendCurl($id, $thingUserName, $thingPassword)
+    {
+        // TODO VICTOR: endpoint y puerto pasarlo como parámetro al constructor. Definirlo en parámetros y en services, Usarlo como servicio la url y el puerto
         // Esto q es tan variable, definirlo conf/ en parámetros, o hacerlo en services
-        $ch = curl_init(self::ENDPOINT . '/' . $id);
+        $ch = curl_init($this->endpoint . '/' . $id);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_PORT, 8001);
+        curl_setopt($ch, CURLOPT_PORT, $this->port);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'user: ' . $thing_user_name,
-                'password: ' . $thing_password
+                'user: ' . $thingUserName,
+                'password: ' . $thingPassword
             )
         );
 
@@ -34,30 +50,30 @@ class CurlThingConnectedRepository implements ThingConnectedRepository
         return json_decode($json);
     }
 
-    public function searchThingByIdOrException(int $id, string $thing_user_name, string $thing_password)
+    public function searchThingByIdOrException(int $id, string $thingUserName, string $thingPassword)
     {
-        $thingConnected = $this->sendCurl($id, $thing_user_name, $thing_password);
+        $thingConnected = $this->sendCurl($id, $thingUserName, $thingPassword);
         if (isset($thingConnected->error)) {
             throw new \Exception("Error while connecting to thing " . $thingConnected->error);
         }
         return $thingConnected;
     }
 
-    public function searchThingNameByIdOrException(int $id, string $thing_user_name, string $thing_password)
+    public function searchThingNameByIdOrException(int $id, string $thingUserName, string $thingPassword)
     {
-        $thingConnected = $this->searchThingByIdOrException($id, $thing_user_name, $thing_password);
+        $thingConnected = $this->searchThingByIdOrException($id, $thingUserName, $thingPassword);
         return $thingConnected->name;
     }
 
-    public function searchThingBrandByIdOrException(int $id, string $thing_user_name, string $thing_password)
+    public function searchThingBrandByIdOrException(int $id, string $thingUserName, string $thingPassword)
     {
-        $thingConnected = $this->searchThingByIdOrException($id, $thing_user_name, $thing_password);
+        $thingConnected = $this->searchThingByIdOrException($id, $thingUserName, $thingPassword);
         return $thingConnected->brand;
     }
 
-    public function searchThingActionsByIdOrException(int $id, string $thing_user_name, string $thing_password)
+    public function searchThingActionsByIdOrException(int $id, string $thingUserName, string $thingPassword)
     {
-        $thingConnected = $this->searchThingByIdOrException($id, $thing_user_name, $thing_password);
+        $thingConnected = $this->searchThingByIdOrException($id, $thingUserName, $thingPassword);
         return $thingConnected->actions;
         return $thingConnected->actions->actions;
         // DUDA hacer esta transformacion de obj a array
