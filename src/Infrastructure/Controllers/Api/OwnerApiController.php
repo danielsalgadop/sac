48f82a4babbdb758daf46c196322700105b09820
daniel salgado <daniel.salgado.p@gmail.com>
2019-06-22 14:18:59 +0200

    added Action to Friend relationship via 'Share' button

--- src/Infrastructure/Controllers/Api/OwnerApiController.php
+++ src/Infrastructure/Controllers/Api/OwnerApiController.php
@@ -4,8 +4,14 @@
 namespace App\Infrastructure\Controllers\Api;
 
 
+use App\Application\Command\Action\SearchActionByIdCommand;
+use App\Application\Command\Friend\SearchFriendByIdCommand;
 use App\Application\Command\Owner\SearchOwnerByFbDelegatedCommand;
+use App\Application\Command\Owner\ShareActionWithFriendCommand;
+use App\Application\CommandHandler\Action\SearchActionByIdHandler;
+use App\Application\CommandHandler\Friend\SearchFriendByIdHandler;
 use App\Application\CommandHandler\Owner\SearchOwnerByFbDelegatedHandler;
+use App\Application\CommandHandler\Owner\ShareActionWithFriendHandler;
 use App\Infrastructure\Controllers\HasFbSessionController;
 use App\Infrastructure\Owner\Serializer\OwnerArraySeralizer;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,20 +23,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class OwnerApiController extends AbstractController implements HasFbSessionController
 {
     private $searchOwnerByFbDelegatedHandler;
+    private $searchFriendByIdHandler;
+    private $searchActionByIdHandler;
+    private $shareActionWithFriendHandler;
 
-    public function __construct(SearchOwnerByFbDelegatedHandler $searchOwnerByFbDelegatedHandler)
+    public function __construct(SearchOwnerByFbDelegatedHandler $searchOwnerByFbDelegatedHandler,SearchFriendByIdHandler $searchFriendByIdHandler, SearchActionByIdHandler $searchActionByIdHandler, ShareActionWithFriendHandler $shareActionWithFriendHandler)
     {
         $this->searchOwnerByFbDelegatedHandler = $searchOwnerByFbDelegatedHandler;
+        $this->searchFriendByIdHandler = $searchFriendByIdHandler;
+        $this->searchActionByIdHandler = $searchActionByIdHandler;
+        $this->shareActionWithFriendHandler = $shareActionWithFriendHandler;
     }
 
-//    public function info($fbDelegated)
     public function info(Request $request)
     {
         $session = $request->getSession();
         $ownerFbDelegated = $session->get('ownerFbDelegated');
-
-        // TODO, securizar este fbDelegated, ahora mismo con que exita en mi bbdd ya lo daré por bueno. Lo ideas sería
-        // asegurar que la persona que me lo manda en realidad ES quien fb ha asignado ese fbDelegated
 
         try {
             $owner = $this->searchOwnerByFbDelegatedHandler->handle(new SearchOwnerByFbDelegatedCommand($ownerFbDelegated));
@@ -39,5 +47,13 @@ class OwnerApiController extends AbstractController implements HasFbSessionContr
             return new JsonResponse($e->getMessage(),401);
         }
         return new JsonResponse(OwnerArraySeralizer::serialize($owner));
+    }
+
+    public function shareActionWithFriend(int $friendId, int $actionId)
+    {
+        $action = $this->searchActionByIdHandler->handle(new SearchActionByIdCommand($actionId));
+        $friend = $this->searchFriendByIdHandler->handle(new SearchFriendByIdCommand($friendId));
+        $this->shareActionWithFriendHandler->handle(new ShareActionWithFriendCommand($friend,$action));
+        return new JsonResponse(true,200);
     }
 }
