2021d17efa3880dd10a8c7d314aa3617e54420c1
daniel salgado <daniel.salgado.p@gmail.com>
2019-06-30 14:41:10 +0200

    $urlForThingConnected and urlForThingInfo are not send from Controller but asked via ajax from info_owner view

--- src/Infrastructure/Thing/Serializer/ThingArraySerializer.php
+++ src/Infrastructure/Thing/Serializer/ThingArraySerializer.php
@@ -13,45 +13,43 @@ class ThingArraySerializer
 {
     public static function serialize(Thing $thing): array
     {
-        list($urlForThingConnected, $urlForThingInfo) = self::getUrlsForThing($thing->getId());
+//        list($urlForThingConnected, $urlForThingInfo) = self::getUrlsForThing($thing->getId());
 
         return [
             'id' => $thing->getId(),
             'root' => $thing->getRoot(),
-            'urlForThingConnected' => $urlForThingConnected,
-            'urlForThingInfo' => $urlForThingInfo,
         ];
     }
 
-    /*
-     * DUDA VICTOR, he probado muchas cosas, pero no consigo usar el Router aqui:
-     *  - Usar servicio con -"@route" como argumento
-     *  - Usar ThingArraySerializer2, que no es STATIC
-//            'urlForThingConnected' => $this->container->get('router')->generate('api_thing_info', ['thingId' => $thing->getId()]),
-     * TODO (cuando este entregado el proyecto) 19 Abril:
-     *     Hacer un singleton del serializador, que por constructor reciviese RouteCollection y en el método el contexto
-     * */
-    public static function getUrlsForThing($thingId)
-    {
-        //        De momento ponlo en el serializador. En el controller NO, que vas a tener que repetir código en caso de que necesites esa funcionalidad en ltro lado
-
-        // DUDA Victor, manera de obterner la ruta
-        $apiRouteYaml = Yaml::parseFile('../src/Infrastructure/Resources/config/routes/api.yaml');
-        $thingRouteYaml = Yaml::parseFile('../src/Infrastructure/Resources/config/routes/thing.yaml');
-
-        $routes = new RouteCollection();
-
-        $routes->add('api_thing_info', new Route($apiRouteYaml['api_thing_info']['path']));
-        $routes->add('thing_info', new Route($thingRouteYaml['thing_info']['path']));
-        $context = new RequestContext();
-
-        $urlGenerator = new UrlGenerator($routes, $context);
-        $urlForThingConnected = $urlGenerator->generate('api_thing_info', ['thingId' => $thingId]);
-        $urlForThingInfo = $urlGenerator->generate('thing_info', ['thingId' => $thingId]);
-
-        return [
-            $urlForThingConnected,
-            $urlForThingInfo
-        ];
-    }
+//    /*
+//     * DUDA VICTOR, he probado muchas cosas, pero no consigo usar el Router aqui:
+//     *  - Usar servicio con -"@route" como argumento
+//     *  - Usar ThingArraySerializer2, que no es STATIC
+////            'urlForThingConnected' => $this->container->get('router')->generate('api_thing_info', ['thingId' => $thing->getId()]),
+//     * TODO (cuando este entregado el proyecto) 19 Abril:
+//     *     Hacer un singleton del serializador, que por constructor reciviese RouteCollection y en el método el contexto
+//     * */
+//    public static function getUrlsForThing($thingId)
+//    {
+//        //        De momento ponlo en el serializador. En el controller NO, que vas a tener que repetir código en caso de que necesites esa funcionalidad en ltro lado
+//
+//        // DUDA Victor, manera de obterner la ruta
+//        $apiRouteYaml = Yaml::parseFile('../src/Infrastructure/Resources/config/routes/api.yaml');
+//        $thingRouteYaml = Yaml::parseFile('../src/Infrastructure/Resources/config/routes/thing.yaml');
+//
+//        $routes = new RouteCollection();
+//
+//        $routes->add('api_thing_info', new Route($apiRouteYaml['api_thing_info']['path']));
+//        $routes->add('thing_info', new Route($thingRouteYaml['thing_info']['path']));
+//        $context = new RequestContext();
+//
+//        $urlGenerator = new UrlGenerator($routes, $context);
+//        $urlForThingConnected = $urlGenerator->generate('api_thing_info', ['thingId' => $thingId]);
+//        $urlForThingInfo = $urlGenerator->generate('thing_info', ['thingId' => $thingId]);
+//
+//        return [
+//            $urlForThingConnected,
+//            $urlForThingInfo
+//        ];
+//    }
 }
