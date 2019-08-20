31ad7ccbd4e349487340e7cb1370dac98680b2b9
danielsalgadop <daniel.salgado.p@gmail.com>
2019-08-20 11:24:35 +0200

    documenting

--- CI/deploy/old_deploy.sh
+++ CI/deploy/old_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-ssh -i "socialaccesscontroller-paris.pem" ubuntu@ec2-35-180-227-177.eu-west-3.compute.amazonaws.com; cd /var/www/sac;
+ssh -i ~/sac_sandbox/docs/socialaccesscontroller-paris.pem ubuntu@ec2-35-180-227-177.eu-west-3.compute.amazonaws.com; cd /var/www/sac;
  git pull origin master
