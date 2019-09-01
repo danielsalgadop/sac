dfb9f8438e1089a5b767a389663c5fa5fd7aa971
daniel.salgado <daniel.salgado.p@gmail.com>
2019-09-01 19:16:37 +0000

    renamed to SearchByFriendId

--- src/Infrastructure/Friend/Command/SearchByFriendId.php
+++ src/Infrastructure/Friend/Command/SearchByFriendId.php
@@ -4,29 +4,37 @@ namespace App\Infrastructure\Friend\Command;
 
 use App\Application\Command\Friend\SearchFriendByIdCommand;
 use App\Application\CommandHandler\Friend\SearchFriendByIdHandler;
+use App\Domain\Entity\Friend;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SearchByFbId extends ContainerAwareCommand
+class SearchByFriendId extends Command
 {
     protected static $defaultName = 'app:Friend:SearchFriendById';
+    private $searchFriendByIdHandler;
+
+    public function __construct(SearchFriendByIdHandler $searchFriendByIdHandler)
+    {
+        parent::__construct();
+        $this->searchFriendByIdHandler = $searchFriendByIdHandler;
+    }
 
     protected function configure()
     {
         $this
             ->setDescription('given id returns friend')
-            ->addArgument('id', InputArgument::REQUIRED, '(int) id')
-        ;
+            ->addArgument('id', InputArgument::REQUIRED, '(int) id');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $id = $input->getArgument('id');
 
-        $friendRepository = $this->getContainer()->get('app.repository.friend');
-        $searchFriendByIdHandler = new SearchFriendByIdHandler($friendRepository);
+        $searchFriendByIdHandler = $this->searchFriendByIdHandler;
+        /** @var $friend Friend */
         $friend = $searchFriendByIdHandler->handle(new SearchFriendByIdCommand($id));
 
         dd($friend);
