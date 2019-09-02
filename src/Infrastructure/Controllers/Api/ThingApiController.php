9c2063bfe11def529e00d3a6136b4bee15978bef
daniel.salgado <daniel.salgado.p@gmail.com>
2019-09-02 14:55:22 +0000

    clean code

--- src/Infrastructure/Controllers/Api/ThingApiController.php
+++ src/Infrastructure/Controllers/Api/ThingApiController.php
@@ -7,13 +7,14 @@ namespace App\Infrastructure\Controllers\Api;
 use App\Application\Command\Thing\MergeThingWithThingConnectedByIdCommand;
 use App\Application\CommandHandler\Thing\MergeThingWithThingConnectedByIdHandler;
 use App\Infrastructure\Controllers\HasFbSessionController;
+use App\Infrastructure\Owner\Serializer\JsonSerializer;
 use App\Infrastructure\Thing\Serializer\ThingWithThingConnectedArraySerializer;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Infrastructure\Owner\Serializer\JsonSerializer;
 
 
-class ThingApiController extends AbstractController  implements HasFbSessionController
+class ThingApiController extends AbstractController implements HasFbSessionController
 {
 
     private $mergeThingWithThingConnectedByIdHandler;
@@ -25,12 +26,11 @@ class ThingApiController extends AbstractController  implements HasFbSessionCont
 
     public function info(int $thingId)
     {
-        file_put_contents("/tmp/debug.txt", __METHOD__ . ' ' . __LINE__ . PHP_EOL . var_export($thingId, true) . PHP_EOL, FILE_APPEND);
         try {
             $thingWithThingConnected = $this->mergeThingWithThingConnectedByIdHandler->handle(new MergeThingWithThingConnectedByIdCommand($thingId));
             return new JsonResponse(ThingWithThingConnectedArraySerializer::serialize($thingWithThingConnected));
-        } catch (\Exception $e) {
-            return new JsonResponse($e->getMessage(), 500);
+        } catch (Exception $e) {
+            return new JsonResponse($e->getMessage(), 400);
         }
 
     }
