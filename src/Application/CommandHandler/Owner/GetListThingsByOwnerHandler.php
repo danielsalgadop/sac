3bf3ce04672b12a0d02f3b0ab7d34b49b430976e
daniel.salgado <daniel.salgado.p@gmail.com>
2019-09-18 00:58:15 +0000

    clean code

--- src/Application/CommandHandler/Owner/GetListThingsByOwnerHandler.php
+++ src/Application/CommandHandler/Owner/GetListThingsByOwnerHandler.php
@@ -4,8 +4,6 @@ namespace App\Application\CommandHandler\Owner;
 
 use App\Application\Command\Owner\GetListThingsByOwnerCommand;
 use App\Domain\Entity\Owner;
-//use App\Domain\Entity\Thing;
-//use App\Domain\Repository\OwnerRepository;
 
 class GetListThingsByOwnerHandler
 {
