d5a98783c5052496c631bceebf0a0a91fa8681a5
danielsalgadop <daniel.salgado.p@gmail.com>
2019-09-18 02:52:42 +0200

    cleaner code

--- src/Application/CommandHandler/Owner/GetListThingsByOwnerHandler.php
+++ src/Application/CommandHandler/Owner/GetListThingsByOwnerHandler.php
@@ -3,27 +3,21 @@
 namespace App\Application\CommandHandler\Owner;
 
 use App\Application\Command\Owner\GetListThingsByOwnerCommand;
-use App\Domain\Entity\Thing;
-use App\Domain\Repository\OwnerRepository;
+use App\Domain\Entity\Owner;
+//use App\Domain\Entity\Thing;
+//use App\Domain\Repository\OwnerRepository;
 
 class GetListThingsByOwnerHandler
 {
-//    public function __construct(OwnerRepository $ownerRepository)
-//    {
-//        $this->ownerRepository = $ownerRepository;
-//    }
-
-    // TODO: determine return type. If file named List must return a list?
     public function handle(GetListThingsByOwnerCommand $getListThingsByOwnerCommand):array
     {
+        /** @var Owner $owner */
         $owner = $getListThingsByOwnerCommand->getOwner();
 
         $things = [];
         foreach ($owner->getThings() as $thing){
             $things[] = $thing;
         }
-        return $things;
-        $things = $owner->getThings();
         return $things;
     }
 }
